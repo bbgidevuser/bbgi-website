fa87a3e359d1b9c314d69c50e3b3662c87ed99fa
Sazi <s_mtandabuzo@yahoo.com>
2020-03-31 17:47:13 +0200

    Add code for supplier database

--- HTML/insert.php
+++ HTML/insert.php
@@ -1,25 +1,51 @@
 <?php
-$username = $_POST['username'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$company = $_POST['company'];
-$industry = $_POST['industry'];
-$message = $_POST['message'];
+header("Content-Security-Policy: script-src 'self' insert.php");
+if(empty($_SERVER['CONTENT_TYPE']))
+{
+  $_SERVER['CONTENT_TYPE'] = "application/x-www-form-urlencoded";
+}
 
-if(!empty($username) || !empty($email) || !empty($phone) || !empty($company) || !empty($industry) || !empty($message)){
+print "CONTENT_TYPE: " . $_SERVER['CONTENT_TYPE'] . "<BR />";
+$data = file_get_contents('php://input');
+print "DATA: <pre>";
+var_dump($data);
+var_dump($_POST);
+print "</pre>";
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+$name = $_REQUEST['name'];
+echo $name;
+$email = $_REQUEST['email'];
+echo $email;
+$phone = $_REQUEST['phone'];
+$company = $_REQUEST['company'];
+$industry = $_REQUEST['industry'];
+$message = $_REQUEST['message'];
+
+//if(isset($_POST(['submit']))){
+if(!empty($name) || !empty($email) || !empty($phone) || !empty($company) || !empty($industry) || !empty($message)){
    $host = "localhost";
-   $dbUsername = "root";
-   $dbPassword = "";
-   $dbname = "bbgi";
+   $dbUsername = "admin";
+   $dbPassword = "admin";
+   $dbname = "id12686733_bbgi";
 
    //create connection
-   $conn = new mysqli_connect($host, $dbUsername, $dbPassword, $dbname);
+   $conn = mysqli_connect($host, $dbUsername, $dbPassword, $dbname);
+    //$conn = mysqli_connect("localhost", "admin", "admin", "id12686733_bbgi");
+    //$conn = new PDO("mysql:host=$host;dbname=$dbname", $dbUsername, $dbPassword);
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    echo "Connected successfully";
 
    if(mysqli_connect_error()){
-     die('Connect Error('. mysqli_connect_errorno().')'.mysqli_connect())
+     die('Connect Error('. mysqli_connect_errorno().')'.mysqli_connect());
    } else {
-    $SELECT = "SELECT username From suppliers Where email = ? Limit 1";
-    $INSERT = "INSERT Into suppliers (username, email, phone, company, industry, message) values(?,?,?,?,?)";
+    $SELECT = "SELECT name From suppliers Where email = ? Limit 1";
+    $INSERT = "INSERT Into suppliers (name, email, phone, company, industry, message) values(?,?,?,?,?)";
 
     //Prepare statement
     $stmt = $conn->prepare($SELECT);
@@ -33,7 +59,7 @@ if(!empty($username) || !empty($email) || !empty($phone) || !empty($company) || 
      $stmt->close();
 
      $stmt = $conn->prepare($INSERT);
-     $stmt->bind_param("ssssii", $username, $email, $phone, $company, $industry, $message);
+     $stmt->bind_param("ssssii", $name, $email, $phone, $company, $industry, $message);
      $stmt->execute();
      echo "New record inserted successfully";
     } else {
@@ -45,5 +71,7 @@ if(!empty($username) || !empty($email) || !empty($phone) || !empty($company) || 
 } else {
   echo "All fields are required";
   die();
+}
+//}
 }
 ?>
