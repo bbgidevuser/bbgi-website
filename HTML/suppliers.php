d659155f2871e34d15d8ea126ab33f40e338f988
Sazi <s_mtandabuzo@yahoo.com>
2020-07-18 10:26:03 +0200

    Supplier page design

---
+++ HTML/suppliers.php
@@ -0,0 +1,283 @@
+<?php include('config.php'); ?>
+<!DOCTYPE html>
+<!-- ==============================
+    Project:        Metronic "Asentus" Frontend Freebie - Responsive HTML Template Based On Twitter Bootstrap 3.3.4
+    Version:        1.0
+    Author:         KeenThemes
+    Primary use:    Corporate, Business Themes.
+    Email:          support@keenthemes.com
+    Follow:         http://www.twitter.com/keenthemes
+    Like:           http://www.facebook.com/keenthemes
+    Website:        http://www.keenthemes.com
+    Premium:        Premium Metronic Admin Theme: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
+================================== -->
+<html lang="en" class="no-js">
+    <!-- BEGIN HEAD -->
+    <head>
+        <meta charset="utf-8"/>
+        <title>Black Business Growth Initiative</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta content="width=device-width, initial-scale=1" name="viewport"/>
+        <meta content="" name="description"/>
+        <meta content="" name="author"/>
+
+        <!-- GLOBAL MANDATORY STYLES -->
+        <link href="http://fonts.googleapis.com/css?family=Hind:300,400,500,600,700" rel="stylesheet" type="text/css">
+        <link href="vendor/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+
+        <!-- PAGE LEVEL PLUGIN STYLES -->
+        <link href="css/animate.css" rel="stylesheet">
+        <link href="vendor/swiper/css/swiper.min.css" rel="stylesheet" type="text/css"/>
+
+        <!-- THEME STYLES -->
+        <link href="css/layout.min.css" rel="stylesheet" type="text/css"/>
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="favicon.ico"/>
+
+        <!--===============================================================================================-->
+        <link rel="icon" type="image/png" href="suppliers/images/icons/favicon.ico"/>
+        <!--===============================================================================================-->
+        <!--<link rel="stylesheet" type="text/css" href="suppliers/vendor/bootstrap/css/bootstrap.min.css">-->
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="suppliers/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="suppliers/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="suppliers/vendor/animate/animate.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="suppliers/vendor/css-hamburgers/hamburgers.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="suppliers/vendor/animsition/css/animsition.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="suppliers/vendor/select2/select2.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="suppliers/vendor/daterangepicker/daterangepicker.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="suppliers/css/util.css">
+        <link rel="stylesheet" type="text/css" href="suppliers/css/main.css">
+        <!--===============================================================================================-->
+
+    </head>
+    <!-- END HEAD -->
+
+    <!-- BODY -->
+    <body>
+
+        <!--========== HEADER ==========-->
+        <header class="header navbar-fixed-top">
+            <!-- Navbar -->
+            <nav class="navbar" role="navigation">
+                <div class="container">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="menu-container">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".nav-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="toggle-icon"></span>
+                        </button>
+
+                        <!-- Logo -->
+                        <div class="logo">
+                            <a class="logo-wrap" href="index.php">
+                                <img class="logo-img logo-img-main" src="img/transparent.png" alt="Asentus Logo">
+                                <img class="logo-img logo-img-active" src="img/transparent.png" alt="Asentus Logo">
+                            </a>
+                        </div>
+                        <!-- End Logo -->
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse nav-collapse">
+                        <div class="menu-container">
+                            <ul class="navbar-nav navbar-nav-right">
+                                <li class="nav-item"><a class="nav-item-child nav-item-hover active" href="index.php">Home</a></li>
+                                <li class="nav-item"><a class="nav-item-child nav-item-hover" href="about.php">About</a></li>
+                                <li class="nav-item"><a class="nav-item-child nav-item-hover" href="tickets.php">Tickets</a></li>
+                                <li class="nav-item"><a class="nav-item-child nav-item-hover" href="events.php">Events</a></li>
+                                <li class="nav-item"><a class="nav-item-child nav-item-hover" href="faq.php">FAQ</a></li>
+                                <li class="nav-item"><a class="nav-item-child nav-item-hover" href="contact.php">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- End Navbar Collapse -->
+                </div>
+            </nav>
+            <!-- Navbar -->
+        </header>
+        <!--========== END HEADER ==========-->
+
+        <!--========== SLIDER ==========-->
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <div class="container">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                </ol>
+            </div>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img class="img-responsive" src="img/1920x1080/17.jpg" alt="Slider Image">
+                    <div class="container">
+                        <div class="carousel-centered">
+                            <div class="margin-b-40">
+                                <!--<h1 class="carousel-title">Black Business Growth Initiative</h1>-->
+                                <!--<p>Lorem ipsum dolor amet consectetur adipiscing dolore magna aliqua <br/> enim minim estudiat veniam siad venumus dolore</p>-->
+                            </div>
+                            <!--<a href="#" class="btn-theme btn-theme-sm btn-white-brd text-uppercase">Explore</a>-->
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <img class="img-responsive" src="img/1920x1080/12.jpg" alt="Slider Image">
+                    <div class="container">
+                        <div class="carousel-centered">
+                            <div class="margin-b-40">
+                                <!--<h2 class="carousel-title">Black Business Growth Initiative</h2>-->
+                                <!--<p>Lorem ipsum dolor amet consectetur adipiscing dolore magna aliqua <br/> enim minim estudiat veniam siad venumus dolore</p>-->
+                            </div>
+                            <!--<a href="#" class="btn-theme btn-theme-sm btn-white-brd text-uppercase">Explore</a>-->
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <img class="img-responsive" src="img/1920x1080/16.jpg" alt="Slider Image">
+                    <div class="container">
+                        <div class="carousel-centered">
+                            <div class="margin-b-40">
+                                <!--<h2 class="carousel-title">Black Business Growth Initiative</h2>-->
+                                <!--<p>Lorem ipsum dolor amet consectetur adipiscing dolore magna aliqua <br/> enim minim estudiat veniam siad venumus dolore</p>-->
+                            </div>
+                            <!--<a href="#" class="btn-theme btn-theme-sm btn-white-brd text-uppercase">Explore</a>-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--========== SLIDER ==========-->
+
+        <!--========== PAGE LAYOUT ==========-->
+        <div class="bg-color-sky-light" data-auto-height="true">
+            <div class="content-lg container">
+
+            </div>
+        </div>
+        <?php include('suppliers/suppliers.php'); ?>
+        <!-- Masonry Grid -->
+        <!--========== END PAGE LAYOUT ==========-->
+        <div class="bg-color-sky-light" data-auto-height="true">
+            <div class="content-lg container">
+
+            </div>
+        </div>
+        <!--========== FOOTER ==========-->
+        <footer class="footer">
+            <!-- Links -->
+            <div class="footer-seperator">
+                <div class="content-lg container">
+                    <div class="row">
+                        <div class="col-sm-2 sm-margin-b-50">
+                            <!-- List -->
+                            <ul class="list-unstyled footer-list">
+                                <li class="footer-list-item"><a class="footer-list-link" href="index.php">Home</a></li>
+                                <li class="footer-list-item"><a class="footer-list-link" href="about.php">About</a></li>
+                                <li class="footer-list-item"><a class="footer-list-link" href="events.php">Events</a></li>
+                                <li class="footer-list-item"><a class="footer-list-link" href="tickets.php">Tickets</a></li>
+                                <li class="footer-list-item"><a class="footer-list-link" href="#">Clients</a></li>
+                                <li class="footer-list-item"><a class="footer-list-link" href="#">Careers</a></li>
+                                <li class="footer-list-item"><a class="footer-list-link" href="contact.php">Contact</a></li>
+                                <li class="footer-list-item"><a class="footer-list-link" href="#">Terms</a></li>
+                                <li class="footer-list-item"><a class="footer-list-link" href="faq.php">FAQ</a></li>
+                            </ul>
+                            <!-- End List -->
+                        </div>
+                        <div class="col-sm-4 sm-margin-b-30">
+                            <!-- List -->
+                            <ul class="list-unstyled footer-list">
+                                <li class="footer-list-item"><a class="footer-list-link" href="#">Twitter</a></li>
+                                <li class="footer-list-item"><a class="footer-list-link" href="https://www.facebook.com/blackbusinessgrowthinitiative/">Facebook</a></li>
+                                <li class="footer-list-item"><a class="footer-list-link" href="#">Instagram</a></li>
+                                <li class="footer-list-item"><a class="footer-list-link" href="#">YouTube</a></li>
+                            </ul>
+                            <!-- End List -->
+                        </div>
+                        <form action="insert-home.php" method="POST">
+                        <div class="col-sm-5 sm-margin-b-30">
+                            <h2 class="color-white">Supplier Database</h2>
+                            <p>It is our passion to nurture and develop black business. Benefit from and service our community by signing up on our supplier database.</p>
+                            <input type="text" name="name" class="form-control footer-input margin-b-20" placeholder="Name" required>
+                            <input type="email" name="email" class="form-control footer-input margin-b-20" placeholder="Email" required>
+                            <input type="text" name="phone" class="form-control footer-input margin-b-20" placeholder="Phone" required>
+                            <input type="text" name="company" class="form-control footer-input margin-b-20" placeholder="Company" required>
+                            <input type="text" name="industry" class="form-control footer-input margin-b-20" placeholder="Industry" required>
+                            <textarea name="message" class="form-control footer-input margin-b-30" rows="6" placeholder="Message" required></textarea>
+                            <button id="submit" name="submit" type="submit" class="btn-theme btn-theme-sm btn-base-bg text-uppercase">Submit</button>
+                        </div>
+                        </form>
+                    </div>
+                    <!--// end row -->
+                </div>
+            </div>
+            <!-- End Links -->
+
+            <!-- Copyright -->
+            <div class="content container">
+                <div class="row">
+                    <div class="col-xs-6">
+                        <img class="footer-logo" src="img/transparent.png" alt="Asentus Logo">
+                    </div>
+                    <div class="col-xs-6 text-right">
+                        <!--<p class="margin-b-0"><a class="color-base fweight-700" href="http://keenthemes.com/preview/asentus/">Asentus</a> Theme Powered by: <a class="color-base fweight-700" href="http://www.keenthemes.com/">KeenThemes.com</a></p>-->
+                    </div>
+                </div>
+                <!--// end row -->
+            </div>
+            <!-- End Copyright -->
+        </footer>
+        <!--========== END FOOTER ==========-->
+
+        <!-- Back To Top -->
+        <a href="javascript:void(0);" class="js-back-to-top back-to-top">Top</a>
+
+        <!-- JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
+        <!-- CORE PLUGINS -->
+        <script src="vendor/jquery-migrate.min.js" type="text/javascript"></script>
+        <script src="vendor/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+
+        <script src="suppliers/vendor/jquery/jquery-3.2.1.min.js"></script>
+
+        <script src="suppliers/vendor/daterangepicker/moment.min.js"></script>
+        <script src="suppliers/vendor/daterangepicker/daterangepicker.js"></script>
+
+        <script src="suppliers/vendor/countdowntime/countdowntime.js"></script>
+
+        <script src="suppliers/vendor/animsition/js/animsition.min.js"></script>
+
+        <script src="suppliers/vendor/bootstrap/js/popper.js"></script>
+
+        <script src="suppliers/vendor/select2/select2.min.js"></script>
+
+        <!-- PAGE LEVEL PLUGINS -->
+        <script src="vendor/jquery.easing.js" type="text/javascript"></script>
+        <script src="vendor/jquery.back-to-top.js" type="text/javascript"></script>
+        <script src="vendor/jquery.smooth-scroll.js" type="text/javascript"></script>
+        <script src="vendor/jquery.wow.min.js" type="text/javascript"></script>
+        <script src="vendor/swiper/js/swiper.jquery.min.js" type="text/javascript"></script>
+        <script src="vendor/masonry/jquery.masonry.pkgd.min.js" type="text/javascript"></script>
+        <script src="vendor/masonry/imagesloaded.pkgd.min.js" type="text/javascript"></script>
+
+        <!-- PAGE LEVEL SCRIPTS -->
+        <script src="js/layout.min.js" type="text/javascript"></script>
+        <script src="js/components/wow.min.js" type="text/javascript"></script>
+        <script src="js/components/swiper.min.js" type="text/javascript"></script>
+        <script src="js/components/masonry.min.js" type="text/javascript"></script>
+
+        <script src="suppliers/js/main.js"></script>
+
+    </body>
+    <!-- END BODY -->
+</html>
